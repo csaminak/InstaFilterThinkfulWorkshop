61d8a50f37bfae1c034d4cbc36e8da46a7f3bbb7
Samina Chhatwal <csaminak@gmail.com>
2017-10-10 19:53:59 -0400

    trying out more filter options

--- index.js
+++ index.js
@@ -4,12 +4,39 @@ function revertFilter() {
   });
 }
 
-function addFilter() {
+function addFilter1() {
   Caman('#bourbon', function() {
     this['hazyDays']();
     this.brightness(-10);
     this.contrast(-5);
     this.vibrance(20);
+    this.render();
+  });
+}
+
+function addFilter2() {
+  Caman('#bourbon', function() {
+    this['sunrise']();
+    // this.brightness(-10);
+    this.render();
+  });
+}
+
+function addFilter3() {
+  Caman('#bourbon', function() {
+    this['hemingway']();
+    this.brightness(-10);
+    this.sharpen(20);
+    this.sepia(30);
+    this.hue(95);
+    this.render();
+  });
+}
+
+function addFilter4() {
+  Caman('#bourbon', function() {
+    this['pinhole']();
+    this.contrast(-20);
     this.render();
   });
 }
