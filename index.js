1f8f4689040712a473da8b054816edc98060c42b
Samina Chhatwal <csaminak@gmail.com>
2017-10-10 19:36:38 -0400

    added a built-in filter with filter adjustments to make bourbon look like how I desire

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function revertFilter() {
 
 function addFilter() {
   Caman('#bourbon', function() {
-    // this.brightness(10);
-    this['jarques']();
+    this['hazyDays']();
+    this.brightness(-10);
+    this.contrast(-5);
+    this.vibrance(20);
     this.render();
   });
 }
